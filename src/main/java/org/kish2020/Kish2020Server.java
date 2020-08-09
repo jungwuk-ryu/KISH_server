b4571b4b3da073d180ca495152ebac6f7acdb476
Hancho1577 <vojougae35@gmail.com>
2020-08-09 22:47:16 +0700

    Organize imports

--- src/main/java/org/kish2020/Kish2020Server.java
+++ src/main/java/org/kish2020/Kish2020Server.java
@@ -2,12 +2,6 @@ package org.kish2020;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
 public class Kish2020Server {
