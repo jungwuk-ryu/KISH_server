3acb0097711ac7f9a4cc31ee3f476768050a1b43
Hancho1577 <vojougae35@gmail.com>
2020-08-22 17:00:33 +0700

    Implement getCalendar api

---
+++ src/main/java/org/kish2020/entity/SchoolCalendar.java
@@ -0,0 +1,55 @@
+package org.kish2020.entity;
+
+import com.google.gson.Gson;
+import org.kish2020.DataBase.DataBase;
+import org.kish2020.MainLogger;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashSet;
+
+public class SchoolCalendar extends DataBase<HashSet<String>> {
+    private static Gson gson = new Gson();
+    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    private String json;
+
+    public SchoolCalendar() {
+        super("schoolCalendarDB.json", true, true);
+        this.commit();
+    }
+
+    public SchoolCalendar add(String date, String event){
+        return this.add(date, event,1);
+    }
+
+    public SchoolCalendar add(String sourceDate, String event, int duration){
+        if(duration < 1){
+            MainLogger.error("", new IllegalArgumentException("duration은 1 이상의 수만 허용됩니다."));
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        try {
+            calendar.setTime(simpleDateFormat.parse(sourceDate));
+        } catch (ParseException e) {
+            MainLogger.error("전달된 date : " + sourceDate, e);
+            return null;
+        }
+        for(int i = 0; i < duration; i++) {
+            calendar.add(Calendar.DAY_OF_MONTH, 1);
+            String newDate = simpleDateFormat.format(calendar.getTime());
+            HashSet<String> schedules = this.getOrDefault(newDate, new HashSet<>());
+            schedules.add(event);
+            this.put(newDate, schedules);
+        }
+        return this;
+    }
+
+    public void commit(){
+        this.json = gson.toJson(this);
+    }
+
+    public String getJson(){
+        return this.json;
+    }
+}
--- src/main/java/org/kish2020/web/ApiServerController.java
+++ src/main/java/org/kish2020/web/ApiServerController.java
@@ -6,7 +6,7 @@ import org.kish2020.DataBase.ExpandedDataBase;
 import org.kish2020.Kish2020Server;
 import org.kish2020.MainLogger;
 import org.kish2020.entity.LunchMenu;
-import org.kish2020.entity.SimplePost;
+import org.kish2020.entity.SchoolCalendar;
 import org.kish2020.utils.WebUtils;
 import org.kish2020.utils.parser.KishWebParser;
 import org.springframework.stereotype.Controller;
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 @Controller
 @RequestMapping("/api")
@@ -28,6 +29,7 @@ public class ApiServerController {
     private final Kish2020Server main;
     private ExpandedDataBase db;
     private ExpandedDataBase lunchLikesDB;
+    private SchoolCalendar calendar;
 
     public ApiServerController(Kish2020Server kish2020Server){
         MainLogger.info("Api Server Controller 초기화중");
@@ -40,6 +42,8 @@ public class ApiServerController {
             e.printStackTrace();
         }
         this.testDatesJson = testDates.toJSONString();
+        this.calendar = new SchoolCalendar();
+        this.makeCalendar();
     }
 
     /**
@@ -89,5 +93,64 @@ public class ApiServerController {
     public @ResponseBody String getExamDates(){
         MainLogger.info("getExamDates 호출");
         return this.testDatesJson;
+    }
+
+    /**
+     * <p>학사일정 API입니다.</p>
+     * <p>http://www.hanoischool.net/?menu_no=41에 학사일정이 없는 관계로
+     * 직접 입력한 데이터를 사용해야합니다...</p>*/
+    @RequestMapping("/getCalendar")
+    public @ResponseBody String getCalendar(){
+        return this.calendar.getJson();
+    }
+
+    private void makeCalendar(){
+        /*학사일정이 홈페이지에 등록되어있지 않기에 직접 입력해줍니다...*/
+        if(calendar.isEmpty()) {
+            calendar.add("2020-08-31", "1학기 종료")
+                    .add("2020-09-01", "2학기 시작")
+                    .add("2020-09-02", "외국어의 날")
+                    .add("2020-09-04", "인문학에세이쓰기공모전")
+                    .add("2020-09-11", "2학기 간부수련회")
+                    .add("2020-09-12", "과학캠프")
+                    .add("2020-09-14", "2학기 방과후 수업 시작")
+                    .add("2020-09-21", "스포츠리그(스포츠교류)", 1)
+                    .add("2020-10-01", "추석")
+                    .add("2020-10-02","추석연휴")
+                    .add("2020-10-06", "10-11학년 학부모 대상\n교육과정 및 진학설명회")
+                    .add("2020-10-09", "한글날 행사")
+                    .add("2020-10-20", "1차 지필평가", 3)
+                    .add("2020-10-26", "스포츠클럽주간", 5)
+                    .add("2020-10-30", "과학탐구포트폴리오")
+                    .add("2020-10-30", "프리젠테이션발표대회")
+                    .add("2020-11-04", "10학년 수학여행", 3)
+                    .add("2020-11-05", "8학년 수학여행", 2)
+                    .add("2020-11-09", "화재예방 및 재난대피 훈련주간", 5)
+                    .add("2020-11-6", "현장체험학습")
+                    .add("2020-11-18", "2차 학생 건강검진")
+                    .add("2020-11-20", "KISH 문화제")
+                    .add("2020-11-21", "KISH 음악회")
+                    .add("2020-11-26", "총학생회 유세")
+                    .add("2020-11-27", "총학생회 선거")
+                    .add("2020-11-30", "교육과정 만족도조사")
+                    .add("2020-12-01", "예비중∙고학생 OT")
+                    .add("2020-12-04", "독서퀴즈대회")
+                    .add("2020-12-15", "2차 지필평가", 4)
+                    .add("2020-12-19", "전편입생 지필면접")
+                    .add("2020-12-21", "스포츠클럽주간", 8)
+                    .add("2020-12-24", "인문학에세이쓰기공모전")
+                    .add("2020-12-25", "크리스마스")
+                    .add("2020-12-31", "졸업진급사정회(4교시)")
+                    .add("2021-01-01", "신정" )
+                    .add("2021-01-07", "(오전) 종업식(2교시)")
+                    .add("2021-01-07", "(오후) 졸업식")
+                    .add("2021-01-08", "중등교사출근일")
+                    .add("2021-01-08", "겨울방학(예정)", 24 + 28)
+                    .add("2021-01-11", "방학 중 방과후 수업", 12)
+                    .add("2021-02-06", "설날연휴(예정)",9)
+                    .add("2021-02-22", "전교직원 출근일", 4)
+                    .add("2021-02-23", "전교직원 워크숍", 2)
+                    .commit();
+        }
     }
 }
