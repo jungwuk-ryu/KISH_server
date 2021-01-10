21a616e84309f81c1f834d55be599e8d994248d4
Hancho1577 <vojougae35@gmail.com>
2021-01-10 22:51:39 +0700

    misc: Add timestamp to Exam
    시험 일정 정보에 timestamp값을 포함시킵니다

--- src/main/java/org/kish/entity/Exam.java
+++ src/main/java/org/kish/entity/Exam.java
@@ -3,16 +3,24 @@ package org.kish.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Getter
 @Setter
 public class Exam {
+    static SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd");
+
     private int id;
     private long timestamp;
     private String label;
+    private String date;
 
     public Exam(int id, long timestamp, String label){
+        Date date = new Date(timestamp * 1000);
         setId(id);
         setTimestamp(timestamp);
         setLabel(label);
+        setDate(SDF.format(date));
     }
 }
